ecd3ecfca2c0ae0e89710362bd7dbd0c5c2b4139
Benz56 <bemmamin@live.dk>
2022-12-22 08:58:13 +0100

    Hardcoded api key.

--- Frontend/src/components/GoogleMap.tsx
+++ Frontend/src/components/GoogleMap.tsx
@@ -285,7 +285,7 @@ const MapComponent = () => {
 
 const GoogleMap = () => {
   return (
-    <Wrapper apiKey={process.env.GOOGLE_MAPS_API_KEY ?? ""} render={render}>
+    <Wrapper apiKey={"AIzaSyAvDFB5J9Yh5wPS0ABAobkhxp-PwVRdhrk"} render={render}>
       <MapComponent />
     </Wrapper>
   );
